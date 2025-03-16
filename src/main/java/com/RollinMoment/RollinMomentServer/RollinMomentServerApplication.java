74ed9e301839159b1376debe7aa30cd0b8b2153b
baesaa0304 <baesaa0304@naver.com>
2025-03-16 12:43:54 +0900

    feat: Git 형상관리 세팅

---
+++ src/main/java/com/RollinMoment/RollinMomentServer/RollinMomentServerApplication.java
@@ -0,0 +1,13 @@
+package com.RollinMoment.RollinMomentServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollinMomentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollinMomentServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RollinMoment/RollinMomentServer/RollinMomentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RollinMoment.RollinMomentServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollinMomentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
