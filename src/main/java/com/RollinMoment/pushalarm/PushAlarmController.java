ef0db5bcfa9c6b12bb9388793ba72f56e4c25dc8
sylee <kelly61099@gmail.com>
2025-03-23 23:41:36 +0900

    feat: push alarm api 생성 시작

---
+++ src/main/java/com/RollinMoment/pushalarm/PushAlarmController.java
@@ -0,0 +1,10 @@
+package com.RollinMoment.pushalarm;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PushAlarmController {
+	// TODO :: push alarm api
+}
---
+++ src/main/java/com/RollinMoment/pushalarm/PushAlarmService.java
@@ -0,0 +1,18 @@
+package com.RollinMoment.pushalarm;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PushAlarmService {
+
+	//TODO :: PushAlarm
+	public void sendPushAlarm() {
+		// 1. google api
+		// 2. firebase api
+
+		// 해당 작업에 push token 필요
+		// push token -> 사용자 특정 (회원 테이블에 push token)
+	}
+}
