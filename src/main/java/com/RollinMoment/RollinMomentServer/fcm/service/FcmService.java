ecc2d359a02a319cc169ff9aca4ba0c0619ba108
sylee <s0young.lee.147@gmail.com>
2025-03-26 13:17:35 +0900

    dev: alarm ios token 수정

--- src/main/java/com/RollinMoment/RollinMomentServer/fcm/service/FcmService.java
+++ src/main/java/com/RollinMoment/RollinMomentServer/fcm/service/FcmService.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class FcmService {
-	// TODO :: syPushToken == 소영 폰 푸시토큰임. user 회원가입 등 진행 후 삭제
+	// TODO :: syPushToken == Android(소영) 푸시토큰 / jhPushToken == iOS(재현) 푸시토큰 -> userId 적용 후 삭제 필요
 	private final String syPushToken = "fLEr_JbXRtK-yRRrwYfF7z:APA91bFFWhbdcUMAxR9C86yQp7iiBVr1haWve2np__xeiF3V60f1jtNWlNJeXJ7tKf4H_1MDN7Zw6wFRgdiAdDCmohb4pGpHK7kT-plBVFpCRSV56KnzlRk";
-	private final String jhPushToken = "382478f2bd4fd97609f16a1c33bafeada9a13b0b9ff471ad8e4784b712e8b955";
+	private final String jhPushToken = "fuoLaWI3r08Il2uBGdauyU:APA91bE2wEXH_JzPbFG5BJH6V17FsmwMPlgh9Q2o-kschkRl8rsLF4BICAxqEYRUh1cDsyZVzPMe-yIaSmADDSjWlhG-nMH0D5UCsyyDbPz12UQj6GTlYsA";
 
 //	private final UserRepository userRepository;
 
@@ -20,39 +20,56 @@ public class FcmService {
 		String token = getToken(request.userId());
 
 		Message message = Message.builder()
-								  .setToken(token)
-								  .setNotification(
-										  Notification.builder()
-												  .setTitle(request.title())
-												  .setBody(request.content())
-												  .build()
-								  )
-								  .setAndroidConfig(
-										  AndroidConfig.builder()
-												  .setNotification(
-														  AndroidNotification.builder()
-																  .setTitle(request.title())
-																  .setBody(request.content())
-																  .setClickAction("push_click")
-																  .build()
-												  )
-												  .build()
-								  )
-								  .setApnsConfig(
-										  ApnsConfig.builder()
-												  .setAps(Aps.builder()
-																  .setCategory("push_click")
-																  .build())
-												  .build()
-								  )
-								  .build();
+									.putData("location", "ex.moment")
+									.putData("locationType", "ex.inApp")
+									.putData("dataKey", "ex.momentCode")
+									.setToken(token)
+									.setNotification(
+											Notification.builder()
+													.setTitle(request.title())
+													.setBody(request.content())
+													.build()
+									)
+									.setAndroidConfig(TokenAndroidConfig(request))
+									.setApnsConfig(TokenApnsConfig(request))
+								.build();
+
 
 		try {
 			String response = FirebaseMessaging.getInstance().send(message);
 			log.info("FCMsend-"+response);
 		} catch (FirebaseMessagingException e) {
-			log.info("FCMexcept-"+ e.getMessage());
+			log.error("FCMexcept-"+ e.getMessage());
 		}
+	}
+
+	// Android 세팅
+	public AndroidConfig TokenAndroidConfig(FcmPushAlertRequest request) {
+		return AndroidConfig.builder()
+//				.setCollapseKey(request.getCollapseKey())
+				.setNotification(AndroidNotification.builder()
+						.setTitle(request.title())
+						.setBody(request.content())
+						.setClickAction("push_click")
+						.build())
+				.build();
+	}
+
+	// APNs 세팅 ( iOS )
+	public ApnsConfig TokenApnsConfig(FcmPushAlertRequest request) {
+		return ApnsConfig.builder()
+				.setAps(Aps.builder()
+						.setAlert(
+								ApsAlert.builder()
+										.setTitle(request.title())
+										.setBody(request.content())
+//										.setLaunchImage(request.getImgUrl())
+										.build()
+						)
+//						.setCategory(request.getCollapseKey())
+						.setSound("default")
+						.build())
+				.build();
 	}
 
 	// TODO :: token userId 기반으로 찾아서 사용
